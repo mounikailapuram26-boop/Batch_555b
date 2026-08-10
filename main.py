118764ad4bbc2c14baaf88963b50063996d496f3
mounikailapuram26-boop <mounikailapuram26@gmail.com>
2026-08-10 11:21:40 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+app = FastAPI(title="Batch 555B API")
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/name")
+def read_name():
+    return {"name": "Batch 55B"}
+
+@app.get("/batch")
+def read_batch():
+    return {"batch": "Batch 55B"}
+ 
